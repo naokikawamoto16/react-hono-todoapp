1af3fd40c3a69e30c7db5d7c0cc6e5e75da55347
Naoki Kawamoto <kevinriverbook@gmail.com>
2024-12-18 13:05:40 +0900

    feat: add get task by id and create new task endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,45 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
+interface Task {
+  id: number
+  name: string
+  completed: boolean
+}
+
+const tasks: Task[] = [
+  { id: 1, name: 'Task 1', completed: true },
+  { id: 2, name: 'Task 2', completed: false }
+]
+
 const app = new Hono()
+
+// Get task by id
+app.get('/tasks/:id', (c) => {
+  const id = Number(c.req.param('id'))
+  const task = tasks.find(t => t.id === id)
+  if (!task) {
+    return c.json({ message: 'Task not found' }, 404)
+  }
+  return c.json(task)
+})
 
 // Get all tasks
 app.get('/tasks', (c) => {
   return c.json(tasks)
+})
+
+// Create a new task
+app.post('/tasks', async (c) => {
+  const body = await c.req.json()
+  const id = tasks.length + 1
+  const task = {
+    id,
+    name: body.name,
+    completed: false
+  }
+  tasks.push(task)
+  return c.json({ message: 'success' }, 201)
 })
 
 const port = 3000
@@ -15,14 +49,3 @@ serve({
   fetch: app.fetch,
   port
 })
-
-interface Task {
-  id: number
-  name: string
-  completed: boolean
-}
-
-const tasks: Task[] = [
-  { id: 1, name: 'Task 1', completed: false },
-  { id: 2, name: 'Task 2', completed: true }
-]
